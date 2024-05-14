87826306001ca9a1854d8476d944a5723457692f
Kneatrum <martin.mwiti.n@gmail.com>
2024-05-14 15:37:30 -0400

    getting book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,8 +47,17 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksList = Object.values(books);
+  const booksByTitle = booksList.filter((book) => {
+    return (book.title === title)
+  });
+
+  if(booksByTitle.length > 0){
+    res.send(JSON.stringify({booksByTitle},null,4))
+  } else{
+    return res.status(300).json({message: "Yet to be implemented"});
+  }
 });
 
 //  Get book review
