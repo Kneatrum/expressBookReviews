4abc6dfa44c86ee03a96d8cc3d5e80a6faac3518
Kneatrum <martin.mwiti.n@gmail.com>
2024-05-14 16:06:10 -0400

    Getting a review based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -56,14 +56,21 @@ public_users.get('/title/:title',function (req, res) {
   if(booksByTitle.length > 0){
     res.send(JSON.stringify({booksByTitle},null,4))
   } else{
-    return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(300).json({message: "Book by the provided title does not exist"});
   }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    if(parseInt(isbn)){
+        const book = books[isbn];
+        if(book){
+            res.send(book.reviews)
+        } 
+    } else{
+        return res.status(300).json({message: "Please enter a valid ISBN"});
+    }
 });
 
 function findBookByISBN(isbn){
