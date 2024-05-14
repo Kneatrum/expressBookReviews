f5d2c15c21c2cb0a71764444d3a938aceb3006a4
Kneatrum <martin.mwiti.n@gmail.com>
2024-05-14 15:26:31 -0400

    Getting books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,8 +32,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookList = Object.values(books)
+  let booksByAuthor = bookList.filter((book) => {
+    return (book.author === author)
+  });
+  if(booksByAuthor.length > 0){
+    res.send(JSON.stringify({booksByAuthor},null,4));
+  } else{
+      return res.status(300).json({message: "Books by this author not found"});
+  }
+  
 });
 
 // Get all books based on title
